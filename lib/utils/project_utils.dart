98b224b5c1f7405e9640fbf624d3c69b28ccaa10
mihir <mihir.arora@gmail.com>
2022-06-29 19:34:20 +0530

    update

--- lib/utils/project_utils.dart
+++ lib/utils/project_utils.dart
@@ -2,42 +2,33 @@ class ProjectUtils {
   static const List<String> banners = [
     "assets/projects/portfolio.png",
     "assets/projects/new.png",
-    "assets/projects/medkitB.png",
-    "assets/projects/hereiamB.png",
+    "assets/projects/realtime.png",
 
   ];
 
   static const List<String> icons = [
     "assets/projects/flutter.png",
     "assets/projects/flutter.png",
-    "assets/projects/medkit.png",
-    "assets/projects/hereiam.png",
-    "assets/projects/covid.png",
+    "assets/projects/flutter.png",
 
   ];
 
   static const List<String> titles = [
     "Portfolio Website in flutter",
     "Nursery App",
-    "MedKit",
-    "Here I Am",
-    "COVID-19",
+    "REAL TIME FACE RECOGNITION",
+
   ];
 
   static const List<String> description = [
     "A portfolio website to tell who am I and what I do!",
-    "A Nursery App built with Firebase Authentications SignIn/SignOut/Login along with CRED operations with Firebase REST API to place orders and cart",
-    "A Phramacy app developed using Flutter powered with Firebase as database with Doctor and Patient panels.",
-    "Here I am is an Alert app that Sends alert SMS holding your location (Address and Google Maps) to your loved ones.",
-
+    "A Nursery App built with Firebase Authentications SignIn/SignOut/Login along with CRED operations with Firebase REST API to place orders and cart!",
+    "A Real time face recognition built using machine learning algorithms ti built a real time face detection to detect a person's save it and reuse the training data to detect him! "
   ];
 
   static const List<String> links = [
     "https://github.com/mihirarora28/Flutter-PortFolio",
   "https://github.com/mihirarora28/ShopApp2",
-    "https://github.com/mhmzdev/MedKit-Pharmacy-App-Using-Flutter",
-    "https://github.com/mhmzdev/Here-I-Am-Alert-App",
-    "https://github.com/mhmzdev/Covid19-Tracker-App",
-
+    "https://github.com/mihirarora28/Real-Time-Face-Recognition",
   ];
 }
